a30f0bf818c4afebb280e1887ee168bf3dd1225c
Allen Shaw <allen@emphanos.com>
2013-07-02 10:08:17 -0500

    Initial commit.

---
+++ civicrm/Smarty/plugins/function.dsm.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * $Id: function.dsm.php 3 2012-07-26 16:49:50Z as $
+ */
+
+
+    /*
+    * Smarty plugin
+    * -------------------------------------------------------------
+    * Type:     function
+    * Name:     dsm
+    * Purpose:  wrapper for Drupal6 dsm() function (devel module)
+    * -------------------------------------------------------------
+    *
+
+    * Parameters
+    *    var = the variable to dump
+    */
+    function smarty_function_dsm($params, &$smarty) {
+        if (!array_key_exists('var', $params)) {
+            // ref does not exists!
+            $smarty->trigger_error("Plugin dsm: missing required parameter 'var'. (". var_export($params, true) .")");
+            return;
+        }
+
+        if(!function_exists('dsm')) {
+            $smarty->trigger_error("Plugin dsm: function dsm() is not available.  Is devel module enabled?");
+            return;
+        }
+
+        dsm($params['var']);
+    }
---
+++ functions.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * $Id: functions.php 7 2012-07-26 17:44:43Z as $
+ */
+
+/** Defined in hook_menu, callback to create module settings form
+ */
+function _tmtools_settings() {
+  $form['tm_maintenance_time'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Maintenance shutdown time'),
+    '#default_value' => variable_get('maintenance_time', ''),
+    '#description' => t('A time (less than 24 hours in the future) at which you intent to shut down this site for maintenance; should be in server time zone ('. date('T') .'). Format: "HH:MM".'),
+    '#element_validate' => array('_validate_setting_shutdown_time'),
+  );
+  return system_settings_form($form);
+}
+
+/**
+ * Check that a value is a 24-hour time between now and midnight in format HH:MM
+ * @param array $element Drupal form element
+ * @param array $form_state Drupal form state
+ * @return bool TRUE on success, FALSE on failure
+ */
+function _validate_setting_shutdown_time($element, $form_state) {
+  if (empty($element['#value'])) {
+    return true;
+  }
+  if (! preg_match('/[012][0-9]:[0-9]{2}/', $element['#value'])) {
+    form_error($element, 'Must be in 24-hour time format MM:HH');
+  }
+
+  if (time() > strtotime($element['#value'])) {
+    form_error($element, 'Time must be between now and midnight tonight.');
+  }
+  return FALSE;
+}
+
+/**
+ * Helper function to get parsed query without running it. (Modified code
+ * from db_query().
+ */
+function tm_queryp($query) {
+  return "The function tm_queryp is disabled in Drupal 7. Use devel.module's dpq() instead.";
+}
+
+/**
+ * Initialize tmtools
+ */
+function _tmtools_civicrm_initialize() {
+  static $initialized;
+
+  if (!$initialized) {
+
+    civicrm_initialize();
+
+    $tmtools_path = realpath(dirname(__FILE__));
+
+    // also fix php include path
+    $include_path = $tmtools_path . '/civicrm' . PATH_SEPARATOR . get_include_path();
+    set_include_path($include_path);
+
+    $template =& CRM_Core_Smarty::singleton();
+
+    $template->template_dir = array_merge(array($tmtools_path . '/civicrm/Smarty/templates'), (array)$template->template_dir);
+    $template->plugins_dir = array_merge(array($tmtools_path . '/civicrm/Smarty/plugins'), (array)$template->plugins_dir);
+
+    $initialized = TRUE;
+  }
+
+  return $initialized;
+}
